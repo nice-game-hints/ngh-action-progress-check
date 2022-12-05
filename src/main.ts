c985882d96e88757b7ed9da2ea626e68f56aa3df
Juho Rutila <juho.rutila@gmail.com>
2022-12-05 13:23:11 +0200

    feat: run

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,33 @@
 import * as core from '@actions/core'
+import { validateProgress } from './ngh-progress-validator'
 import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const workspaceRoot = process.env['GITHUB_WORKSPACE'] || process.cwd()
+    const mdGlob = core.getInput('mdGlob') || '**/*.md'
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    core.info('check ngh links')
+    core.info(workspaceRoot)
+    core.info(mdGlob)
+    const validationResults = await validateProgress(workspaceRoot, mdGlob)
+    const invalidResults = validationResults
+      .filter(res => !res.valid)
+      .map(res => res.filePath)
+    const invalidFiles =
+      invalidResults.length > 0 ? invalidResults.join(',') : ''
+    core.setOutput('invalidFiles', invalidFiles)
 
-    core.setOutput('time', new Date().toTimeString())
+    if (invalidResults.length > 0) {
+      core.warning(`Invalid Files: ${invalidFiles}`)
+      core.setFailed(
+        'NGH progress validation failed on one or more YAML files.'
+      )
+    } else {
+      core.info(`✅ NGH progress validation completed successfully`)
+    }
   } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+    core.setFailed(error.message)
   }
 }
 
