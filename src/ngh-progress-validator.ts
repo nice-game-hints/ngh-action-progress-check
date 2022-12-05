56a58f54f2926cb20aa88f303b4386a80e557d22
Juho Rutila <juho.rutila@gmail.com>
2022-12-05 13:53:45 +0200

    better logging

--- src/ngh-progress-validator.ts
+++ src/ngh-progress-validator.ts
@@ -3,6 +3,7 @@ import * as path from 'path'
 import * as fs from 'fs'
 import * as core from '@actions/core'
 import {glob} from 'glob'
+import { connected } from 'process'
 const yaml = require('js-yaml')
 
 const readdir = promisify(fs.readdir);
@@ -13,22 +14,18 @@ export interface ValidationResult {
 }
 
 async function findStatus(dir: string): Promise<string[]> {
-	console.log(dir)
 	const subdirs = dir.split(path.sep)
-	console.log(subdirs)
 
 	for (let i = subdirs.length; i > 0; i--) {
-		console.log('subdir:', i, subdirs.slice(0, i + 1))
 		const statusFileLocation = path.join(...subdirs.slice(0, i+1), '_status.yml')
-		console.log(statusFileLocation)
 		try {
 			const statusFile = await fs.promises.readFile(statusFileLocation)
 			const statusYaml = yaml.load(statusFile)
 			const statuses = Object.values(statusYaml).map(sv => Object.keys(sv as {})[0])
-			console.log(statuses)
+			core.debug('found statuses in ' + statusFileLocation)
+			core.debug(statuses.join(' - '))
 			return statuses
 		} catch (nofound) {
-			console.log(statusFileLocation)
 		}
 	}
 	throw 'No status found for ' + dir
@@ -57,27 +54,25 @@ export const validateProgress = async (
 			}
 		)
 	})
-	core.debug('de')
-	console.log(filePaths.join(' - '))
 	core.info(filePaths.join(' - '))
 
 	return await Promise.all(
 		filePaths.map(async filePath => {
-			console.log(filePath)
+			core.info('check statuses in ' + filePath)
 			const allowedStatuses = await findStatus(path.join(workspaceRoot, path.dirname(filePath)))
-			console.log(allowedStatuses)
 			const r = /\(\((when|until) (.*?)\)\)(.*?)\(\(\/(when|until)\)\)/gs
 			const mdFile = fs.readFileSync(path.join(workspaceRoot, filePath)).toString('utf-8')
 			const m = mdFile.matchAll(r)
 			for (const match of m) {
 				const verb = match[1]
 				const state = match[2]
-				console.log(verb, state)
+				core.debug(` found: ${verb} ${state}`)
 				if (!allowedStatuses.includes(state)) {
 					core.warning(`invalid state ${state} in ${filePath}`)
 					return { filePath, valid: false }
 				}
 			}
+			core.debug(` was ok`)
 			return { filePath, valid: true }
 		})
 	)
